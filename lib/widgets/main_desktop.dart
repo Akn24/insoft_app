594cf105715e1f15f70f8966b215a8d5e067ab97
Ankit <ankitnath1999@gmail.com>
2024-06-25 16:54:55 -0400

    [ADD] Added expanded widget in mainDesktopPage

--- lib/widgets/main_desktop.dart
+++ lib/widgets/main_desktop.dart
@@ -16,32 +16,38 @@ class LandingPage extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                "Hi, \nI'm Ankit Kumar Nath\nA Flutter Developer",
-                style: TextStyle(
-                    height: 1.5,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 30.0,
-                    color: CustomColor.whitePrimary),
-              ),
-              const SizedBox(
-                height: 15,
-              ),
-              SizedBox(
-                width: 250,
-                child: ElevatedButton(
-                    onPressed: () {}, child: const Text("Get in touch")),
-              )
-            ],
+          Expanded(
+            flex: 1,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "Hi, \nI'm Ankit Kumar Nath\nA Flutter Developer",
+                  style: TextStyle(
+                      height: 1.5,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30.0,
+                      color: CustomColor.whitePrimary),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                SizedBox(
+                  width: 250,
+                  child: ElevatedButton(
+                      onPressed: () {}, child: const Text("Get in touch")),
+                )
+              ],
+            ),
           ),
-          SvgPicture.asset(
-            "assets/tally-prime-logo.svg",
-            semanticsLabel: 'My SVG Image',
-            height: 100,
-            width: 70,
+          Expanded(
+            flex: 1,
+            child: SvgPicture.asset(
+              "assets/tally-prime-logo.svg",
+              semanticsLabel: 'My SVG Image',
+              height: 100,
+              width: 70,
+            ),
           ),
         ],
       ),
